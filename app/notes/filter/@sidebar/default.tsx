349c8bc8b03fddd0765aa38caa90734b623032b3
Serhii Zhelobkov <meeelnik@gmail.com>
2025-07-06 14:58:43 +0300

    Start

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,17 +1,21 @@
+'use client';
+
 import Link from 'next/link';
 import { tags } from '../../../../constants/tags';
 import css from './SideBarNotes.module.css';
+import { usePathname } from 'next/navigation';
 
-interface SidebarNotesProps {
-  activeTag: string;
-}
+// interface SidebarNotesProps {
+//   activeTag: string;
+// }
 
-const SidebarNotes = ({ activeTag }: SidebarNotesProps) => {
-  const allTags = tags.includes('All') ? tags : ['All', ...tags];
+const SidebarNotes = () => {
+  const path = usePathname().split('/');
+  const activeTag = path[path.length - 1];
 
   return (
     <ul className={css.menuList}>
-      {allTags.map(tag => {
+      {tags.map(tag => {
         const isActive = tag === activeTag;
         return (
           <li key={tag} className={css.menuItem}>
--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -7,12 +7,13 @@ import NoteList from '@/components/NoteList/NoteList';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
 import Pagination from '@/components/Pagination/Pagination';
-import Modal from '@/components/Modal/Modal';
-import NoteForm from '@/components/NoteForm/NoteForm';
+// import Modal from '@/components/Modal/Modal';
+// import NoteForm from '@/components/NoteForm/NoteForm';
 import { useDebounce } from 'use-debounce';
 // import Logo from "@/components/Logo/Logo";
 import type { FetchNotesResponse } from '@/lib/api';
 import { Tag } from '@/types/note';
+import Link from 'next/link';
 
 interface NotesClientProps {
   initialNotesData: FetchNotesResponse;
@@ -25,7 +26,7 @@ export default function NotesClient({
 }: NotesClientProps) {
   const [inputValue, setInputValue] = useState<string>('');
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  // const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   //FETCHING & SEARCHING NOTES
   const [debouncedInputValue] = useDebounce(inputValue, 500);
@@ -56,12 +57,16 @@ export default function NotesClient({
           </div>
           {/* <Logo /> */}
 
-          <button
+          {/* <button
             onClick={() => setIsModalOpen(true)}
             className={css.addbutton}
           >
             Create note +
           </button>
+        </div> */}
+          <Link href={'/notes/action/create'} className={css.addbutton}>
+            Create note +
+          </Link>
         </div>
 
         {/* -------NOTELIST--------- */}
@@ -76,12 +81,12 @@ export default function NotesClient({
         )}
 
         {/* -------NOTE MODAL--------- */}
-
+        {/* 
         {isModalOpen && (
           <Modal onClose={() => setIsModalOpen(false)}>
             <NoteForm onClose={() => setIsModalOpen(false)} />
           </Modal>
-        )}
+        )} */}
       </div>
     </>
   );
